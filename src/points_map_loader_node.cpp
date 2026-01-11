e91cfb5aa039517594b0305a29f030a92c44842c
Herman Singh Umrao <63940157+hermanumrao@users.noreply.github.com>
2026-01-11 13:55:44 +0530

    fixed depriciated include in points_map_loader_node.cpp

--- src/points_map_loader_node.cpp
+++ src/points_map_loader_node.cpp
@@ -6,7 +6,7 @@
 #include <pcl_conversions/pcl_conversions.h>
 
 #include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 
